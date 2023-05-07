1d3d2504daaa929b85b565133cf01d8defa71fb8
ruhuseyn <huseyinovrufet49@gmail.com>
2023-05-07 15:37:01 +0400

    Changes

--- src/main/java/com/bankproject/controllers/UserController.java
+++ src/main/java/com/bankproject/controllers/UserController.java
@@ -4,12 +4,20 @@ import com.bankproject.dtos.UserDto;
 import com.bankproject.entities.User;
 import com.bankproject.serviceImpls.UserImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,12 +25,35 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserImpl userImpl;
+    private final RestTemplate restTemplate;
+
 
     @PostMapping()
-    public boolean checkUser(UserDto userDto){
+    public boolean checkUser(UserDto userDto) {
         userImpl.checkUser(userDto);
-        return true;
+        try {
+            URL url = new URL("91.102.161.166:5000/predict?data=user");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            System.out.println(in.read());
+            in.close();
+            in.readLine();
+            if(in.readLine() == "1"){
+                return true;
+            }
+            else{
+                return false;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
-
-
 }
--- src/main/java/com/bankproject/dtos/UserDto.java
+++ src/main/java/com/bankproject/dtos/UserDto.java
@@ -2,19 +2,19 @@ package com.bankproject.dtos;
 
 import jakarta.persistence.Column;
 
-public record UserDto (
+public record UserDto(
 
-    Integer age,
-    Double salary,
-
-    String ownership,
-
-    Double employmentTime,
-
-    String loanPurposes,
-
-    Double creditAmount,
-
-    String education
-){
+        Integer age,
+        Double salary,
+        String ownership,
+        Double employmentTime,
+        String loanPurposes,
+        Double creditAmount,
+        String education,
+        Double loanRate,
+        Integer creditStatus,
+        Double loanPercentage,
+        Integer paymentHistory,
+        Integer creditHistoryLength
+) {
 }
